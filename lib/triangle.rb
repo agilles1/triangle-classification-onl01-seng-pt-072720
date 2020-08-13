b5a6a8dc3e072d976058d5fd356afc29460be463
agilles1 <adam.g.tpt@gmail.com>
2020-08-13 15:20:17 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :triangle, :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
+    @triangle = [a, b, c]
+  end
+
+  def pos_triangle?
+    @triangle.all? {|sides| sides.positive?}
+  end
+
+  def eq_triangle?
+    if a + b <= c || b + c <= a || c + a <= b
+      false
+    else
+      true
+    end
+  end
+
+  def kind
+    if !pos_triangle? || !eq_triangle?
+      begin
+        raise TriangleError
+      end
+    elsif a == b && b == c
+      :equilateral
+    elsif a != b && b != c && a != c
+      :scalene
+    else 
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    
+  end
 end
+
